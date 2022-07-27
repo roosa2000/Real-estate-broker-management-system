fcaf6487f1ef6ad6801c3f951f4ae7bbd53f8b5c
roosa2000 <62498698+roosa2000@users.noreply.github.com>
2022-07-27 15:07:32 +0530

    Add files via upload

---
+++ backend/src/main/java/com/cg/entity/Customer.java
@@ -0,0 +1,76 @@
+package com.cg.entity;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+/***************************************************************************
+ * 
+ * @author 			ROOSA MOZOOMDAR
+ * Description 		It is an entity class that provides all the fields of User
+ * @version			1.0
+ * @since			23-MAR-2021
+ ***************************************************************************/
+
+@Entity
+public class Customer extends User {
+
+	private String custName;
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "custId")
+	private List<Property> properties;
+
+	public Customer() {
+
+	}
+
+	public Customer(String custName, List<Property> properties) {
+		this.custName = custName;
+		this.properties = properties;
+	}
+	/*******************************************
+	 * Method			Getter Method
+	 * Description 		To get name of Customer
+	 * @returns String	Customer name 
+	 * Created By		ROOSA MOZOOMDAR
+	 * Created Date		23-MAR-2021
+	 *******************************************/
+	public String getCustName() {
+		return custName;
+	}
+	/*******************************************
+	 * Method			Setter Method
+	 * Description 		To set name of Customer
+	 * @param custName  Customer Name
+	 * @returns void
+	 * Created By		ROOSA MOZOOMDAR
+	 * Created Date		23-MAR-2021
+	 *******************************************/
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	/*******************************************
+	 * Method			Getter Method
+	 * Description 		To get all Customers
+	 * @returns List	list of all Properties
+	 * Created By		ROOSA MOZOOMDAR
+	 * Created Date		23-MAR-2021
+	 *******************************************/
+	public List<Property> getProperties() {
+		return properties;
+	}
+	/*******************************************
+	 * Method			 Setter Method
+	 * Description 		 To set name of Customer
+	 * @param properties List of all properties
+	 * @returns void
+	 * Created By		 ROOSA MOZOOMDAR
+	 * Created Date		 23-MAR-2021
+	 *******************************************/
+	public void setProperties(List<Property> properties) {
+		this.properties = properties;
+	}
+
+}
