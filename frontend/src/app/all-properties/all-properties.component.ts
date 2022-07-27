d5a1c44f08bf9c7bbe43f945f2d77cd3265a2d28
roosa2000 <62498698+roosa2000@users.noreply.github.com>
2022-07-27 17:44:33 +0530

    Add files via upload

---
+++ frontend/src/app/all-properties/all-properties.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PropertyServiceService } from '../property-service.service';
+
+@Component({
+  selector: 'app-all-properties',
+  templateUrl: './all-properties.component.html',
+  styleUrls: ['./all-properties.component.css'],
+})
+export class AllPropertiesComponent implements OnInit {
+  properties: any;
+  constructor(
+    private pService: PropertyServiceService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
+    this.pService.getAllProperties().subscribe((data) => {
+      this.properties = data;
+    });
+  }
+
+  delete(id: number) {
+    if(confirm("Are you sure you want to delete?")){
+      this.pService.deleteProperty(id).subscribe((data) => {
+        this.getAll();
+      });
+    }
+    else{
+      this.getAll();
+    }
+    
+  }
+}
