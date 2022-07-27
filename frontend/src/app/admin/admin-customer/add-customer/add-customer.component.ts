96d11404fc8daf7a2eb412bb929ba214af78e2e5
roosa2000 <62498698+roosa2000@users.noreply.github.com>
2022-07-27 17:33:32 +0530

    Add files via upload

---
+++ frontend/src/app/admin/admin-customer/add-customer/add-customer.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerServiceService } from 'src/app/customer-service.service';
+
+@Component({
+  selector: 'app-add-customer',
+  templateUrl: './add-customer.component.html',
+  styleUrls: ['./add-customer.component.css']
+})
+export class AddCustomerComponent implements OnInit {
+
+  customerForm: FormGroup
+  constructor(private fb:FormBuilder, private cService:CustomerServiceService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.customerForm=this.fb.group({
+      custName:['',[Validators.required, Validators.pattern("[a-zA-Z]{3,}$")]],
+      email:['',[Validators.required, Validators.pattern("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")]],
+      password:['',[Validators.required, Validators.pattern("^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$")]],
+      mobile:['',[Validators.required, Validators.pattern("^[7-9][0-9]{9}$")]],
+      city:['',Validators.required]
+    })
+  }
+
+  get custName(){
+    return this.customerForm.get('custName');
+  }
+  get email(){
+    return this.customerForm.get('email');
+  }
+  get password(){
+    return this.customerForm.get('password');
+  }
+  get mobile(){
+    return this.customerForm.get('mobile');
+  }
+  get city(){
+    return this.customerForm.get('city');
+  }
+
+  create(){
+    this.customerForm.value.Role="customer"
+    this.cService.addCustomer(this.customerForm.value).subscribe(res=>{
+      this.router.navigate(['/admin/customer/all'])
+      alert("Customer added Succefully")
+    }, err=>{
+      alert("User with given Email or Mobile number already exists. Please try with different details")
+    })
+    
+  }
+
+}
