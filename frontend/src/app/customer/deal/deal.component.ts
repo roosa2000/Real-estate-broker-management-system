09d3316d3722ea97801506d406e5e030f4e026d8
roosa2000 <62498698+roosa2000@users.noreply.github.com>
2022-07-27 19:48:45 +0530

    Add files via upload

---
+++ frontend/src/app/customer/deal/deal.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DealServiceService } from 'src/app/deal-service.service';
+import { PropertyServiceService } from 'src/app/property-service.service';
+import { UserServiceService } from 'src/app/user-service.service';
+
+@Component({
+  selector: 'app-deal',
+  templateUrl: './deal.component.html',
+  styleUrls: ['./deal.component.css']
+})
+export class DealComponent implements OnInit {
+  propId:number;
+  prop:any;
+  deal:any;
+  constructor(private actRouter:ActivatedRoute,private pService:PropertyServiceService, private dService:DealServiceService, private router:Router, private uService:UserServiceService) { }
+
+  ngOnInit(): void {
+    this.propId=this.actRouter.snapshot.params['propid'];
+    this.pService.getPropertyById(this.propId).subscribe(data=>{
+      this.prop=data
+    })
+  }
+
+  makeDeal(){
+    this.deal={
+      customer: {
+        userId: this.uService.getId()
+      },
+      dealCost: this.prop.offerCost,
+      property: {
+        propId: this.propId
+      }
+    }
+    this.dService.addDeal(this.deal).subscribe(ref=>{
+      this.router.navigate(['customer/myprops/']);
+      alert("You have succesfully brought this property")
+    })
+  }
+
+}
