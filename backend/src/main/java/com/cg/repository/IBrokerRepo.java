579f8e731f41453a031ae0fdc69f161223ef1de8
roosa2000 <62498698+roosa2000@users.noreply.github.com>
2022-07-27 15:22:09 +0530

    Add files via upload

---
+++ backend/src/main/java/com/cg/repository/IBrokerRepo.java
@@ -0,0 +1,18 @@
+package com.cg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/********************************************************************************************
+ * @author		T. CHAITANYA
+ * Description	It is an interface extending JPA Repository for using already defined methods
+ * Version		1.0
+ * Created Date	24-MAR-2021
+ ********************************************************************************************/
+
+import com.cg.entity.Broker;
+
+@Repository
+public interface IBrokerRepo extends JpaRepository<Broker, Integer> {
+
+}
---
+++ backend/src/main/java/com/cg/repository/ICustomerRepo.java
@@ -0,0 +1,18 @@
+package com.cg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.entity.Customer;
+
+/********************************************************************************************
+ * @author		ROOSA MOZOOMDAR
+ * Description	It is an interface extending JPA Repository for using already defined methods
+ * @version		1.0
+ * @since		24-MAR-2021
+ ********************************************************************************************/
+
+@Repository
+public interface ICustomerRepo extends JpaRepository<Customer, Integer> {
+
+}
